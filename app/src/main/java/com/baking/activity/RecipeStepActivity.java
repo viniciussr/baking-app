1d5d8793e5d660309094cee35f92ec3379e48812
Vinicius Rocha <viniciussr@gmail.com>
2018-02-13 15:50:50 -0200

    Bugfix - home button in step activity

--- app/src/main/java/com/baking/activity/RecipeStepActivity.java
+++ app/src/main/java/com/baking/activity/RecipeStepActivity.java
@@ -2,6 +2,7 @@ package com.baking.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.baking.R;
 
@@ -14,5 +15,18 @@ public class RecipeStepActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_step);
         ButterKnife.bind(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
